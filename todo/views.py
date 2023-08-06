94adbb62aeba0814940aab26db3a8fc02abab912
erfan-rd226 <erfan.rajabzade0226@gmail.com>
2023-08-06 14:30:28 +0330

    write todo views

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from .serializers import TodoSerializers
+from .models import Todo
+
+@api_view(['GET'])
+class all_todos(request = Request):
+    todos = Todo.objects.order_by('priority').all()
+    todo_serializer = TodoSerializers(todos , many=True)
+    return Request(todo_serializer.data,status.HTTP_200_OK)
