d65f1b9e24f570e02aa98cf4bf67e1fe0c13adc1
erfan-rd226 <erfan.rajabzade0226@gmail.com>
2023-08-06 13:48:30 +0330

    added status and api_view

--- home/views.py
+++ home/views.py
@@ -3,12 +3,15 @@ from todo.models import Todo
 from django.http import HttpRequest , JsonResponse
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 def index_paje(request):
     context = {'todos' : Todo.objects.order_by('priority').all()
     }           
     return render(request,'home/index.html',context)
 
+@api_view(['GET'])
 def todos_lsan(request = Request):
     todos = list(Todo.objects.order_by('priority').all().values('title','is_done'))
-    return Response({'todos':todos})
+    return Response({'todos':todos}, status.HTTP_200_OK)
