b4cacc9fb9cdcdd34402de0748f38ce136b879e7
erfan-rd226 <erfan.rajabzade0226@gmail.com>
2023-08-12 13:40:52 +0330

    added user serializer

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,7 +1,18 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Todo
+
+User = get_user_model()
 
 class TodoSerializers(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = '__all__' 
+
+
+class UserSerializers(serializers.ModelSerializer):
+    todos = TodoSerializers(read_only=True , many=True)
+
+    class Meta:
+        model = User
+        fields = '__all__'
